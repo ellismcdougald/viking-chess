2bca6883497c83adfb85a88f8759695aea21a49f
Ellis McDougald <ecmcdougald@gmail.com>
2024-02-17 20:32:55 -0700

    Sets up board tests

---
+++ src/tests/board_tests.cpp
@@ -0,0 +1,13 @@
+#ifndef BOARD_TESTS_CPP // GUARD
+#define BOARD_TESTS_CPP
+
+#include"iostream"
+#include <catch2/catch_test_macros.hpp>
+
+#include "../Board.hpp"
+
+TEST_CASE("Test board", "[board]") {
+  Board board;
+}
+
+#endif
