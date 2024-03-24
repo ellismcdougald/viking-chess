61bc5794f5585866a02f5fdd30db4b2f59f3e8e8
Ellis McDougald <ecmcdougald@gmail.com>
2024-03-23 19:12:35 -0600

    Update

--- src/tests/move_tests.cpp
+++ src/tests/move_tests.cpp
@@ -51,4 +51,32 @@ TEST_CASE("Testing move implementation on d4e5 capture", "[move]") {
   }
 }
 
+TEST_CASE("testget_row_col_from_position") {
+  Move move('a', 1, 'a', 2, 0);
+  SECTION("a1") {
+    std::string res = move.get_row_col_from_position(position_string_to_bitboard("a1"));
+    REQUIRE(res == "a1");
+  }
+
+  SECTION("d4") {
+    std::string res = move.get_row_col_from_position(position_string_to_bitboard("d4"));
+    REQUIRE(res == "d4");
+  }
+
+  SECTION("h8") {
+    std::string res = move.get_row_col_from_position(position_string_to_bitboard("h8"));
+    REQUIRE(res == "h8");
+  }
+}
+
+TEST_CASE("test bitboard constructor") {
+  SECTION("test 1") {
+    Move move(position_string_to_bitboard("d4"), position_string_to_bitboard("d5"), 0);
+
+    REQUIRE(move.get_origin() == position_string_to_bitboard("d4"));
+    REQUIRE(move.get_destination() == position_string_to_bitboard("d5"));
+    REQUIRE(move.get_flags() == 0);
+  }
+}
+
 #endif
