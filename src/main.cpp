33f1e6a0aeffd18db310a4cda31fa0ea270f9247
Ellis McDougald <ecmcdougald@gmail.com>
2024-05-12 17:29:54 -0600

    Call run_game

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,8 @@
 #include "Game.hpp"
 
 int main() {
-  /*
-  Board board;
-  board.initialize_board_starting_position();
-  board.print();
-  */
-  
-  
   Game game;
-
   game.run_game();
-  
   
   return 0;
 }
-
