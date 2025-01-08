90dcaf7e415a41f6fa61890478697c0a10badfb2
Ellis McDougald <ecmcdougald@gmail.com>
2025-01-08 14:18:10 -0700

    Replaces move vectors with MoveList

--- src/MoveGenerator.cpp
+++ src/MoveGenerator.cpp
@@ -2,16 +2,17 @@
 #define MOVE_GENERATOR_CPP // GUARD
 
 #include "MoveGenerator.hpp"
+#include "MoveList.hpp"
 
 MoveGenerator::MoveGenerator() {}
 
-std::vector<Move> MoveGenerator::generate_legal_moves(Board &board, Color color) {
-  std::vector<Move> legal_moves;
+MoveList MoveGenerator::generate_legal_moves(Board &board, Color color) {
+  MoveList legal_moves;
   
-  std::vector<Move> pseudo_legal_moves = generate_pseudo_legal_moves(board, color);
+  MoveList pseudo_legal_moves = generate_pseudo_legal_moves(board, color);
   for(int i = 0; i < pseudo_legal_moves.size(); i++) {
     if(board.is_move_legal(pseudo_legal_moves[i], color)) {
-      legal_moves.push_back(pseudo_legal_moves[i]);
+      legal_moves.add_move(pseudo_legal_moves[i]);
     }
   }
 
@@ -20,8 +21,8 @@ std::vector<Move> MoveGenerator::generate_legal_moves(Board &board, Color color)
   return legal_moves;
 }
 
-std::vector<Move> MoveGenerator::generate_pseudo_legal_moves(Board &board, Color color) {
-  std::vector<Move> pseudo_legal_moves;
+MoveList MoveGenerator::generate_pseudo_legal_moves(Board &board, Color color) {
+  MoveList pseudo_legal_moves;
   add_pseudo_legal_pawn_moves(board, color, pseudo_legal_moves);
   add_pseudo_legal_piece_moves(board, color, KNIGHT, pseudo_legal_moves);
   add_pseudo_legal_piece_moves(board, color, BISHOP, pseudo_legal_moves);
@@ -31,7 +32,7 @@ std::vector<Move> MoveGenerator::generate_pseudo_legal_moves(Board &board, Color
   return pseudo_legal_moves;
 }
 
-void MoveGenerator::add_pseudo_legal_pawn_moves(Board &board, Color color, std::vector<Move> &moves) {
+void MoveGenerator::add_pseudo_legal_pawn_moves(Board &board, Color color, MoveList& moves) {
   bitboard promotion_file = color == WHITE ? RANK_8 : RANK_1;
   
   bitboard moving_pawns = board.get_piece_positions(PAWN, color);
@@ -63,7 +64,7 @@ void MoveGenerator::add_pseudo_legal_pawn_moves(Board &board, Color color, std::
   add_pseudo_legal_en_passant_moves(board, color, moves);
 }
 
-void MoveGenerator::add_pseudo_legal_piece_moves(Board &board, Color color, Piece piece, std::vector<Move> &moves) {
+void MoveGenerator::add_pseudo_legal_piece_moves(Board &board, Color color, Piece piece, MoveList& moves) {
   bitboard moving_pieces = board.get_piece_positions(piece, color);
   bitboard other_pieces = board.get_all_piece_positions(color);
   bitboard opposing_pieces = board.get_all_piece_positions(negate_color(color));
@@ -78,7 +79,7 @@ void MoveGenerator::add_pseudo_legal_piece_moves(Board &board, Color color, Piec
   }
 }
 
-void MoveGenerator::add_pseudo_legal_en_passant_moves(Board &board, Color color, std::vector<Move> &moves) {
+void MoveGenerator::add_pseudo_legal_en_passant_moves(Board &board, Color color, MoveList& moves) {
   Color other_color = negate_color(color);
   
   if(board.is_moves_empty(other_color)) return;
@@ -99,16 +100,16 @@ void MoveGenerator::add_pseudo_legal_en_passant_moves(Board &board, Color color,
   while (attack_pawns) {
     bitboard origin_square = pop_lsb(attack_pawns);
     Move move(origin_square, destination_square, 5);
-    moves.push_back(move);
+    moves.add_move(move);
   }
 }
 
-void MoveGenerator::add_legal_castle_moves(Board &board, Color color, std::vector<Move> &moves) {
+void MoveGenerator::add_legal_castle_moves(Board &board, Color color, MoveList& moves) {
   add_legal_kingside_castle_move(board, color, moves);
   add_legal_queenside_castle_move(board, color, moves);
 }
 
-void MoveGenerator::add_legal_kingside_castle_move(Board &board, Color color, std::vector<Move> &moves) {
+void MoveGenerator::add_legal_kingside_castle_move(Board &board, Color color, MoveList& moves) {
   // if color has no castle rights, don't add move
   if(!board.get_can_castle_king(color)) return;
 
@@ -135,10 +136,10 @@ void MoveGenerator::add_legal_kingside_castle_move(Board &board, Color color, st
 
   // otherwise, add move
   Move king_side_castle_move(king_position, king_side_king_destination, 2);
-  moves.push_back(king_side_castle_move);
+  moves.add_move(king_side_castle_move);
 }
 
-void MoveGenerator::add_legal_queenside_castle_move(Board &board, Color color, std::vector<Move> &moves) {
+void MoveGenerator::add_legal_queenside_castle_move(Board &board, Color color, MoveList& moves) {
   // if color has no castle rights, don't add move
   if(!board.get_can_castle_queen(color)) return;
   
@@ -167,24 +168,24 @@ void MoveGenerator::add_legal_queenside_castle_move(Board &board, Color color, s
 
   // otherwise, add move
   Move queen_side_castle_move(king_position, queen_side_king_destination, 2);
-  moves.push_back(queen_side_castle_move);
+  moves.add_move(queen_side_castle_move);
 }
 
-void MoveGenerator::add_moves(bitboard origin, bitboard all_destinations, char flag, std::vector<Move> &moves) {
+void MoveGenerator::add_moves(bitboard origin, bitboard all_destinations, char flag, MoveList& moves) {
   while (all_destinations) {
     bitboard current_destination = pop_lsb(all_destinations);
     Move move(origin, current_destination, flag);
-    moves.push_back(move);
+    moves.add_move(move);
   }
 }
 
-void MoveGenerator::add_promotion_moves(bitboard origin, bitboard all_destinations, bool capture, std::vector<Move> &moves) {
+void MoveGenerator::add_promotion_moves(bitboard origin, bitboard all_destinations, bool capture, MoveList& moves) {
   int start_flag = capture ? 12 : 8;
   while (all_destinations) {
     bitboard current_destination = pop_lsb(all_destinations);
     for (int i = 0; i < 4; i++) {
       Move move(origin, current_destination, start_flag + i);
-      moves.push_back(move);
+      moves.add_move(move);
     }
   }
 }
@@ -194,7 +195,7 @@ uint64_t MoveGenerator::perft(int depth, Board &board, Color color) {
 
   if(depth == 0) return 1;
   
-  std::vector<Move> legal_moves = generate_legal_moves(board, color);
+  MoveList legal_moves = generate_legal_moves(board, color);
   int n_moves = legal_moves.size();
 
   for(int i = 0; i < n_moves; i++) {
@@ -219,7 +220,7 @@ std::array<uint64_t, 3> MoveGenerator::detailed_perft(int depth, Board &board, C
   uint64_t quiet_moves = 0;
   uint64_t capture_moves = 0;
 
-  std::vector<Move> legal_moves = generate_legal_moves(board, color);
+  MoveList legal_moves = generate_legal_moves(board, color);
   int n_moves = legal_moves.size();
 
   for(int i = 0; i < n_moves; i++) {
@@ -239,7 +240,7 @@ uint64_t MoveGenerator::divide(int depth, Board &board, Color color) {
   if(depth == 0) {
     return 1;
   }
-  std::vector<Move> legal_moves = generate_legal_moves(board, color);
+  MoveList legal_moves = generate_legal_moves(board, color);
   uint64_t num_moves = legal_moves.size();
 
   uint64_t nodes, node_total;
@@ -259,7 +260,7 @@ uint64_t MoveGenerator::divide(int depth, Board &board, Color color) {
 }
 
 uint64_t MoveGenerator::fast_perft(int depth, Board &board) {
-  std::vector<Move> legal_moves = generate_legal_moves(board, board.get_turn_color());
+  MoveList legal_moves = generate_legal_moves(board, board.get_turn_color());
   int num_moves = legal_moves.size();
 
   uint64_t nodes = 0;
@@ -276,5 +277,23 @@ uint64_t MoveGenerator::fast_perft(int depth, Board &board) {
 
   return nodes;
 }
+
+uint64_t MoveGenerator::pl_perft(int depth, Board& board) {
+  if (depth == 0) return 1;
+  
+  MoveList pseudo_legal_moves = generate_pseudo_legal_moves(board, board.get_turn_color());
+  int num_moves = pseudo_legal_moves.size();
+  uint64_t nodes = 0;
+
+  for (int i = 0; i < pseudo_legal_moves.size(); i++) {
+    board.execute_move(pseudo_legal_moves[i]);
+    if (!board.is_checked(negate_color(board.get_turn_color()))) {
+      nodes += pl_perft(depth - 1, board);
+    }
+    board.undo_move(pseudo_legal_moves[i]);
+  }
+  return nodes;
+}
+
 
 #endif
