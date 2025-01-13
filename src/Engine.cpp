507d730b9868a11abd4f7ed9ef969ad2eef118d6
Ellis McDougald <ecmcdougald@gmail.com>
2025-01-12 19:22:50 -0700

    Implements play_move and search_best_move functions

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -3,15 +3,55 @@
  */
 
 #include "Engine.hpp"
+#include "MoveList.hpp"
 
 #ifndef ENGINE_CPP // GUARD
 #define ENGINE_CPP // GUARD
 
-Engine::Engine() {}
+Engine::Engine() {
+  time_divider = 50;
+}
 
 bool Engine::set_position(std::string fen_string) {
-  // TODO
+  return board.initialize_fen(fen_string);
+}
+
+bool Engine::play_move(std::string move_str) {
+  MoveList pseudo_legal_moves = move_gen.generate_pseudo_legal_moves(board, board.get_turn_color());
+  for (int i = 0; i < pseudo_legal_moves.size(); ++i) {
+    if (move_str == pseudo_legal_moves[i].to_uci_notation()) {
+      board.execute_move(pseudo_legal_moves[i]);
+      if (board.is_checked(negate_color(board.get_turn_color()))) {
+	board.undo_move(pseudo_legal_moves[i]);
+	return false;
+      }
+      return true;
+    }
+  }
   return false;
+}
+
+void Engine::search_best_move() {
+  unsigned search_time = get_time_for_move();
+  search.alpha_beta_max_root(-10000, 10000, 5, board, move_gen, eval, search_time);
+  Move move = search.get_best_move();
+  std::cout << "bestmove " << move.to_uci_notation() << std::endl;
+}
+
+unsigned Engine::get_time_for_move() {
+  if (moves_to_go == 0) {
+    if (board.get_turn_color() == WHITE) {
+      return white_increment + (white_time - white_time / time_divider) / time_divider;
+    } else {
+      return black_increment + (black_time - black_time / time_divider) / time_divider;
+    }
+  } else {
+    if (board.get_turn_color() == WHITE) {
+      return white_increment + (white_time - white_time / (moves_to_go + 3)) / (0.7 * moves_to_go + 3);
+    } else {
+      return black_increment + (black_time - black_time / (moves_to_go + 3)) / (0.7 * moves_to_go + 3);
+    }
+  }
 }
 
 #endif // GUARD
--- src/Engine.hpp
+++ src/Engine.hpp
@@ -23,19 +23,48 @@ public:
    * Sets up the position described in fen on the board
    * Returns true if position set up successfully, false otherwise
    */
-  bool set_position(std::string fen_str); // TODO
+  bool set_position(std::string fen_str);
 
   /*
    * Plays the given move on the board.
    * Move should be given in long algebraic notation e.g. e2e4, e1g1, e7e8q
    */
-  bool play_move(std::string move_str); // TODO
+  bool play_move(std::string move_str);
+
+  /*
+   * Finds the best engine move.
+   */
+  void search_best_move();
+
+
+  inline void show_board() { board.print(); }
+
+  unsigned get_time_for_move();
+    
+
+
+  // Setters:
+  inline void set_white_time(unsigned wtime) { white_time = wtime; }
+  inline void set_black_time(unsigned btime) { black_time = btime; }
+  inline void set_white_increment(unsigned winc) { white_increment = winc; }
+  inline void set_black_increment(unsigned binc) { black_increment = binc; }
+  inline void set_moves_to_go(unsigned movestogo) { moves_to_go = movestogo; }
 
 private:
   Board board;
   MoveGenerator move_gen;
   Search search;
   Evaluation eval;
+
+  unsigned time_divider;
+
+  // all in ms
+  unsigned white_time;
+  unsigned black_time;
+  unsigned white_increment; // increment per move
+  unsigned black_increment;
+  unsigned moves_to_go; // moves to go until next time control
+  
 };
 
 #endif // GUARD
